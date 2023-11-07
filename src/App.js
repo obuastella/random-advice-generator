4a9fbb7e2f7f02c9e8001c06f85282b8edec48f3
Stella <stellaobua123@gmail.com>
2023-11-07 01:49:16 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  function fetchAdvce() {
+    const meet = axios.get("https://api.adviceslip.com/advice");
+    console.log(meet);
+  }
+
+  function handleClick() {
+    console.log("Button clicked");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World {fetchAdvce}</h1>
     </div>
   );
 }
