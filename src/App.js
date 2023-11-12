cc3f52f721f353bb5dce50528505b93411fb623e
Stella <stellaobua123@gmail.com>
2023-11-12 06:02:13 +0100

    Added the images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { Component, useState } from "react";
 import "./App.css";
-import axios from "axios";
 
 function App() {
-  const [advice, setAdvice] = useState("");
-
-  function fetchAdvce() {
-    const meet = axios.get("https://api.adviceslip.com/advice");
-    console.log(meet);
-  }
-
-  function handleClick() {
-    console.log("Button clicked");
-  }
   return (
-    <div>
-      <h1>Hello World {fetchAdvce}</h1>
+    <div className="card">
+      <p>Adivce #1</p>
+      <h2>This is an Advice</h2>
+      <img src="./images/pattern-divider-desktop.svg" alt="divider" />
     </div>
   );
 }
