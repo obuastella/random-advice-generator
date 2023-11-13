5364d027f88c5e91b4a63bec89447b9a6729deb7
Stella <stellaobua123@gmail.com>
2023-11-13 23:10:45 +0100

    Added axiox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
-
+import React, { useEffect, useState } from "react";
+import divderDesktop from "./images/pattern-divider-desktop.svg";
+import divderMobile from "./images/pattern-divider-mobile.svg";
+import dice from "./images/icon-dice.svg";
+import axios from "axios";
 function App() {
+  const [adivce, setAdvice] = useState("NO advice yet");
+
+  const getAdvice = async () => {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    const advice = await response.data.slip;
+    setAdvice(advice);
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
   return (
     <div className="card">
-      <p>Adivce #1</p>
-      <h2>This is an Advice</h2>
-      <img src="./images/pattern-divider-desktop.svg" alt="divider" />
+      <p>Adivce #{adivce.id}</p>
+      <h2>{adivce.advice}</h2>
+      <img src={divderDesktop} className="divider-desktop" alt="divider" />
+      <img src={divderMobile} className="divider-mobile" alt="divider" />
+
+      <div className="dice" onClick={getAdvice}>
+        <img src={dice} alt="dice" />
+      </div>
     </div>
   );
 }
